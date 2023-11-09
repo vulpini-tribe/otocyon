cbbd6126d62d17ed7e2308c8133949f460adba08
Alena Dzhukich <github@alena.red>
2023-11-09 21:31:54 +0600

    fix: ✨ Add leads' list formatter

--- src/crm/leads/_formatters.rs
+++ src/crm/leads/_formatters.rs
@@ -1,9 +1,67 @@
 use super::_types::{Lead, LeadFormatted, LeadFormattedList};
 
+use crate::service::format_money::format_money;
+use crate::service::format_phone::format_phone;
+
 impl Lead {
+    fn monetary(&self) -> String {
+        let currency = self.currency.as_deref().unwrap_or("USD");
+        let monetary_amount = self.monetary_amount.unwrap_or(0);
+
+        format_money(monetary_amount, currency)
+    }
+
+    fn get_primary_email(&self) -> String {
+        match &self.emails {
+            Some(emails) => {
+                let primary_email = emails
+                    .into_iter()
+                    .find(|email| email.r#type == Some("primary".to_string()));
+
+                let primary_email = match primary_email {
+                    Some(email) => Some(email),
+                    None => emails.first(),
+                };
+
+                match primary_email {
+                    Some(email) => email.email.clone().unwrap_or(String::from("")),
+                    None => String::from(""),
+                }
+            }
+            None => String::from(""),
+        }
+    }
+
+    fn get_primary_phone(&self) -> String {
+        match &self.phone_numbers {
+            Some(phone_numbers) => {
+                let primary_phone = phone_numbers
+                    .into_iter()
+                    .find(|phone_number| phone_number.r#type == Some("primary".to_string()));
+
+                let primary_phone = match primary_phone {
+                    Some(phone_number) => Some(phone_number),
+                    None => phone_numbers.first(),
+                };
+
+                match primary_phone {
+                    Some(phone_number) => format_phone(phone_number).unwrap_or(String::from("")),
+                    None => return String::from(""),
+                }
+            }
+            None => String::from(""),
+        }
+    }
+
     pub fn format_list(&self) -> LeadFormattedList {
         let formatted = LeadFormattedList {
             id: self.id.clone(),
+            name: self.name.clone().unwrap_or(String::from("")),
+            monetary_amount: self.monetary(),
+            company_name: self.company_name.clone().unwrap_or(String::from("")),
+            lead_source: self.lead_source.clone().unwrap_or(String::from("")),
+            primary_email: self.get_primary_email(),
+            primary_phone: self.get_primary_phone(),
         };
 
         formatted
--- src/crm/leads/_types.rs
+++ src/crm/leads/_types.rs
@@ -1,15 +1,30 @@
+use crate::types;
 use serde::{Deserialize, Serialize};
 
 #[serde_with::skip_serializing_none]
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct Lead {
     pub id: String,
+    pub name: Option<String>,
+    pub currency: Option<String>,
+    pub monetary_amount: Option<i64>,
+    pub company_name: Option<String>,
+    pub lead_source: Option<String>,
+    pub emails: Option<Vec<types::Email>>,
+    pub phone_numbers: Option<Vec<types::PhoneNumber>>,
+    pub websites: Option<Vec<types::Website>>,
 }
 
 #[serde_with::skip_serializing_none]
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct LeadFormattedList {
     pub id: String,
+    pub monetary_amount: String,
+    pub company_name: String,
+    pub lead_source: String,
+    pub primary_email: String,
+    pub primary_phone: String,
+    pub name: String,
 }
 
 #[serde_with::skip_serializing_none]
